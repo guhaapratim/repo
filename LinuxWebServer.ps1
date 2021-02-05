9c63ef58e7eefd3a6373a31de36164b16f4e469d
guhaapratim <65932947+guhaapratim@users.noreply.github.com>
2021-02-05 19:07:45 +0530

    Add files via upload

---
+++ LinuxWebServer.ps1
@@ -0,0 +1,39 @@
+﻿Configuration LinuxWebServer 
+{
+
+Import-DSCResource -Module nx
+
+Node localhost
+ {
+    nxPackage httpd
+    {
+        Name = "httpd"
+        Ensure = "Present"
+        PackageManager = "Yum"
+    }
+
+    nxFile DocumentRoot
+    {
+        DestinationPath = "/var/www/html/index.html"
+        Contents = "Hello World I'm Hall`n"
+        Ensure = "Present"
+        Type = "File"
+    }
+
+    #Apache Service
+    nxService ApacheService {
+        Name = 'httpd'
+        State = 'running'
+        Enabled = $true
+        Controller = 'systemd'
+    }
+
+    nxScript AddFirewallRuleFirewalld {
+    SetScript = @'
+#!/bin/bash
+firewall-cmd --permanent --add-service http
+firewall-cmd --reload
+'@
+    }
+ }
+}
